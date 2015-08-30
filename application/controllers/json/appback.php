1fefc28fb75d9e4281b6f4baa1746b8cc17880a4
virachart <firstza22@gmail.com>
2015-08-31 03:31:00 +0700

    Application Function not final

--- application/controllers/json/appback.php
+++ application/controllers/json/appback.php
@@ -17,6 +17,8 @@ class Appback extends CI_Controller{
 	}
 
 	public function findinfo(){
+		$arsend = null;
+
 		$info = $this->input->post("info");
 		$data = json_decode($info);
 		$uuid = $data->uuid;
@@ -24,31 +26,118 @@ class Appback extends CI_Controller{
 		$minor = $data->minor;
 		$id = $data->fb_id;
 		// $ar = array('uuid' => $uuid , 'major' => $major , 'minor' => $minor , 'sensoro_type' => '1');
-		$sqlSenType = "select * from sensoro where uuid='".$uuid."' and major='".$major."' and minor='".$minor."' and sensoro_type='1' ";
+		$sqlSenType = "select * from sensoro where uuid='".$uuid."' and major='".$major."' and minor='".$minor."' and sensoro_type='1' and status_sensoro_id = '1' ";
 		// $data['rs'] = $this->db->select("*")->from("sensoro");
 		$rs = $this->db->query($sqlSenType);
 		$data1 = $rs->row_array();
+		$ar=array(
+				"sensoro_id"=>$data1['sensoro_id'],
+				"fb_id"=>$id
+			);
+			$this->db->insert("sensoro_log",$ar);
 		if ($rs->num_rows != 0) {
-			// $data1['rs'] = $rs->row_array();
-			// echo $data1['sensoro_id'];
+			$idSen = $data1['sensoro_id'];
 			
-		}else{
-			// $sqlAddSenLog = ""
+			// SELECT * FROM (info INNER JOIN store ON info.store_id = store.store_id) JOIN sensoro ON info.store_id = sensoro.store_id
+			$sqlInfo = "SELECT * FROM (info INNER JOIN store ON info.store_id = store.store_id) JOIN sensoro ON info.store_id = sensoro.store_id where sensoro_id = '".$idSen."' and info_begin_date < NOW() and info_expire_date >= NOW() and status_store_id = '1' ";
+			// ".$idSen."
+			$rsInfo = $this->db->query($sqlInfo);
+			// echo $this->db->last_query();
+			$dataInfo = $rsInfo->row_array();
+			$cat = $dataInfo['catagory'];
+			$sqlCat = "Select * from user where ".$cat." = '1' ";
+			$rsCat = $this->db->query($sqlCat);
+			if ($rsCat->num_rows != 0) {
+				$arsend = array("info_id"=>$dataInfo['info_id'],
+								"info_name"=>$dataInfo['info_name'],
+								"info_desc"=>$dataInfo['info_descrip'],
+								"info_begin"=>$dataInfo['info_begin_date'],
+								"info_expire"=>$dataInfo['info_expire_date'],
+								"info_pic"=>$dataInfo['info_pic'],
+								"catagory"=>$dataInfo['catagory'],
+								"store_id"=>$dataInfo['store_id'],
+								"store_name"=>$dataInfo['store_name']
+								);
+
+				//insert data to info_log
+				$arInfo = array("info_id"=>$dataInfo['info_id'],
+								"fb_id"=>$id
+							);
+				$this->db->insert("info_log",$arInfo);
+
+			}
+			// echo "<pre>";
+			// echo $dataInfo->store_id;
+			// print_r($dataInfo);
+			// print_r($dataInfo2);
+			// print_r($rsInfo);
+			// echo "</pre>";
 		}
+		echo json_encode($arsend);
 
 	}
 
 
 	public function storedetail(){
+		$arsend = null;
+
+		$jsStore = $this->input->post("store");
+		$arJsStore = json_decode($jsStore);
+		$id = $arJsStore->store_id;
+
+		$sqlStore = "Select * from store where store_id = '".$id."' and status_store_id = '1' ";
+		$rsStore = $this->db->query($sqlStore);
+		$dataStore = $rsStore->row_array();
+		if ($rsStore->num_rows != 0) {
+			$arsend = array("detail"=>$dataStore['detail'],
+							"address"=>$dataStore['address'],
+							"tel"=>$dataStore['tel'],
+							"opentime"=>$dataStore['open_time'],
+							"pic"=>$dataStore['picture_store']
+							);
+		}else{
+			$arsend = array("detail"=>null,
+							"address"=>null,
+							"tel"=>null,
+							"opentime"=>null,
+							"pic"=>null
+							);
+		}
 		
+
+		echo json_encode($arsend);
 	}
 
 	public function fav(){
-		
+		$jsFav = $this->input->post("fav");
+		$arFav = json_decode($jsFav);
+		//var_dump($ar1);
+		$fbid = $arFav->fb_id;
+		$infoid = $arFav->info_id
+		$arInfo = array("info_id"=>$infoid,
+						"fb_id"=>$fbid
+							);
+		$this->db->insert("favorite",$arInfo);
+		$arsend = array("status"=> "finish")
+		echo json_encode($arsend);
 	}
 
 	public function follow(){
-		
+		$jsFol = $this->input->post("fol");
+		$arFol = json_decode($jsFol);
+		//var_dump($ar1);
+		$fbid = $arFav->fb_id;
+		$storeid = $arFol->store_id
+		$sqlFol = "select * from sensoro where store_id = '".$storeid."' and sensoro_type = '1' ";
+		$rsFol = $this->db->query($sqlFol);
+		$dataFol = $rsStore->row_array();
+		$senid = $dataFol['sensoro_id'];
+		$arFol = array("sensoro_id"=>$senid,
+						"fb_id"=>$fbid
+							);
+		$this->db->insert("follow",$arFol);
+		$arsend = array("status"=> "finish")
+		echo json_encode($arsend);
 	}
 
 	public function setapp(){
