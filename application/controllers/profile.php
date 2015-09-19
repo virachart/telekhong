b4d1cc9503e8005def15620f1f13c16fa70da8ee
virachart <firstza22@gmail.com>
2015-09-20 04:06:29 +0700

    profile

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -7,11 +7,36 @@ class Profile extends CI_Controller{
 	}
 
 	public function index(){
-		$this->load->view("profile");
+
+		$ownerid = $this->session->userdata('ownerid');
+		$sqlgetdata = "select * from owner join user on owner.fb_id = user.fb_id where owner_id = '".$ownerid."' ";
+		$data['getuser'] = $this->db->query($sqlgetdata)->row_array();
+		$this->load->view("profile",$data);
 
 	}
 
-	
+	public function edit($id){
+		if ($this->input->post("btsave")!=null) {
+			$aruser=array(
+				"fb_name"=>$this->input->post("name"),
+				"birth"=>$this->input->post("birth"),
+				"sex"=>$this->input->post("sex")
+			);
+			$this->db->where("fb_id",$id);
+			$this->db->update("user",$aruser);
+
+			$ownerid = $this->session->userdata('ownerid');
+			$arowner = array(
+				"owner_tel"=>$this->input->post("tel"),
+				"owner_email"=>$this->input->post("email")
+			);
+			$this->db->where("owner_id",$ownerid);
+			$this->db->update("owner",$arowner);
+			redirect("profile","refresh");
+		}else{
+			redirect("store");
+		}
+	}
 
 }
 
--- application/views/profile.php
+++ application/views/profile.php
@@ -1,9 +1,59 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Profile</title>
 </head>
 <body>
-
+	<h1>Profile</h1>
+	<?php 
+		echo form_open("profile/edit/".$getuser['fb_id']." ");
+	?>
+	<table>
+		<tr>
+			<td> Name : </td>
+			<td><input type="text" name="name" id="name" value="<?php echo $getuser['fb_name']; ?>"> </td>
+		</tr>
+		<tr>
+			<td> Birth Day : </td>
+			<td><input type="date" name="birth" id="birth" class="form-control" value="<?php echo $getuser['birth']; ?>"> </td>
+		</tr>
+		<tr>
+			<td> Gender : </td>
+			<td>
+			<?php
+				$ma;
+				$fe;
+				$ot;
+				if ($getuser['sex'] == "male") {
+					$ma = "selected";
+				}elseif ($getuser['sex'] == "female") {
+					$fe = "selected";
+				}else{
+					$ot = "selected";	
+				}
+			?>
+				<select name="sex" id="sex">
+					<option value="male" <?php echo $ma; ?> > Male </option>
+					<option value="female" <?php echo $fe; ?> > Female </option>
+					<option value="other" <?php echo $ot; ?> > Other </option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td> Telephone Number : </td>
+			<td><input type="text" name="tel" id="tel" value="<?php echo $getuser['owner_tel']; ?>"> </td>
+		</tr>
+		<tr>
+			<td> E-Mail : </td>
+			<td><input type="text" name="tel" id="tel" value="<?php echo $getuser['owner_email']; ?>"> </td>
+		</tr>
+		<tr>
+			<td>&nbsp</td>
+			<td><input type="submit" name="btsave" value="Save"> &nbsp <?php echo anchor("store","Cancle"); ?></td>
+		</tr>
+	</table>
+	<?php 
+		echo form_close();
+	?>
 </body>
 </html>
