7d80327e41ae2a20ce40287d57871918932f0def
virachart <firstza22@gmail.com>
2015-10-25 23:22:26 +0700

    bug

--- application/controllers/manageqrowner.php
+++ application/controllers/manageqrowner.php
@@ -62,19 +62,19 @@ class Manageqrowner extends CI_Controller{
 
 			
 			if ($name != null) {
-				$this->load->library("pagination");
-				$config['base_url'] = base_url()."index.php/manageqr";
-				$config['per_page'] = 10;
-				$sqlnumrow = "SELECT * FROM (qr INNER JOIN info ON qr.info_id = info.info_id) INNER JOIN store ON qr.store_id = store.store_id where store_name like '%".$name."%'";
-				$e = $this->db->query($sqlnumrow);
-				$config['total_rows'] = $e->num_rows();
-				$config['full_tag_open'] = "<div class = 'pagination'>";
-				$config['full_tag_close'] = "</div>";
-				$this->pagination->initialize($config);
+				
 				$sqlqr = "Select * from qr";
 				$data['num1'] = $this->db->query($sqlqr);
-				$data['rs'] = $this->db->select("*")->from("qr a")->join("info b","a.info_id = b.info_id")->join("store c","a.store_id = c.store_id")->like("store_name",$name)->limit($config['per_page'],$this->uri->segment(3))->get()->result_array();
 				
+				$data['rs'] = $this->db->select("*,count(qr_log.qr_log_id) AS qrcount")
+										->from("qr a")
+										->join("info b","a.info_id = b.info_id")
+										->join("store c","a.store_id = c.store_id")
+										->join("qr_log","a.qr_id = qr_log.qr_id","left")
+										->where("status_qr_id !=","3")
+										->like("store_name",$name)
+										->group_by("a.qr_id")
+										->get()->result_array();
 				// echo $this->db->last_query();
 				$this->load->view("manageqrowner",$data);
 				// print_r($data['rs']);
